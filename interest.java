3353c8459b0a32a3f94c1f3cdaac4707d5e4188f
Kartikgupta2006 <kartik6f66@gmail.com>
2024-09-01 17:10:55 +0530

    Add files via upload

---
+++ interest.java
@@ -0,0 +1,17 @@
+ import java.util.*;
+
+public class interest {
+    public static void main(String[] args) {
+       Scanner scan = new Scanner (System.in);
+       System.out.println("Enter the value of principal: ");
+       double principal = scan.nextDouble();
+       System.out.println("Enter the value of rate: ");
+       double rate = scan.nextDouble();
+       System.out.println("Enter the value of time period: ");
+       int time = scan.nextInt();
+       double Time=(int)time;
+       double s;
+        s= (principal*rate*Time)/100;
+        System.out.println("simple interest:" + s);
+    };
+};
